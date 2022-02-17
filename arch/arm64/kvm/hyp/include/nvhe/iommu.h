be5f3392b8e2684e4c10ff71cc94388d2d1adf16
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-02-17 16:33:49 +0000

    KVM: arm64: iommu: Add domains
    The IOMMU domain abstraction allows to share the same page tables
    between multiple devices. That may be necessary due to hardware
    constraints, if multiple devices cannot be isolated by the IOMMU
    (conventional PCI bus for example). It may also help with optimizing
    resource or TLB use. For pKVM in particular, it may be useful to reduce
    the amount of memory required for page tables. All devices owned by the
    host kernel could be attached to the same domain (though that requires
    host changes).
    
    Each IOMMU device holds an array of domains, and the host allocates
    domain IDs that index this array. The alloc() operation initializes the
    domain and prepares the page tables. The attach() operation initializes
    the device table that holds the PGD and its configuration.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- arch/arm64/kvm/hyp/include/nvhe/iommu.h
+++ arch/arm64/kvm/hyp/include/nvhe/iommu.h
@@ -2,8 +2,12 @@
 #ifndef __ARM64_KVM_NVHE_IOMMU_H__
 #define __ARM64_KVM_NVHE_IOMMU_H__
 
+#include <kvm/iommu.h>
+#include <linux/io-pgtable.h>
+
 #if IS_ENABLED(CONFIG_KVM_IOMMU)
 int kvm_iommu_init(void);
+int kvm_iommu_init_device(struct kvm_hyp_iommu *iommu);
 void *kvm_iommu_donate_page(void);
 void kvm_iommu_reclaim_page(void *p);
 
@@ -74,8 +78,20 @@ static inline phys_addr_t kvm_iommu_iova_to_phys(pkvm_handle_t iommu_id,
 }
 #endif /* CONFIG_KVM_IOMMU */
 
+struct kvm_iommu_tlb_cookie {
+	struct kvm_hyp_iommu	*iommu;
+	pkvm_handle_t		domain_id;
+};
+
 struct kvm_iommu_ops {
 	int (*init)(void);
+	struct kvm_hyp_iommu *(*get_iommu_by_id)(pkvm_handle_t smmu_id);
+	int (*alloc_iopt)(struct io_pgtable *iopt, unsigned long pgd_hva);
+	int (*free_iopt)(struct io_pgtable *iopt);
+	int (*attach_dev)(struct kvm_hyp_iommu *iommu, pkvm_handle_t domain_id,
+			  struct kvm_hyp_iommu_domain *domain, u32 endpoint_id);
+	int (*detach_dev)(struct kvm_hyp_iommu *iommu, pkvm_handle_t domain_id,
+			  struct kvm_hyp_iommu_domain *domain, u32 endpoint_id);
 };
 
 extern struct kvm_iommu_ops kvm_iommu_ops;
