a91845ee458a6c78a557211e7227f611586e634e
Vincent Donnefort <vdonnefort@google.com>
2022-08-24 17:57:20 +0100

    KVM: arm64: Add clock support in the nVHE hyp
    By default, the arm64 host kernel is using the arch timer as a source
    for sched_clock. Conveniently, EL2 has access to that same counter,
    allowing to generate clock values that are synchronized.
    
    The clock needs nonetheless to be setup with the same slope values as
    the kernel. Introducing at the same time trace_clock() which is expected
    to be later configured by the hypervisor tracing.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/include/asm/kvm_hyp.h
+++ arch/arm64/include/asm/kvm_hyp.h
@@ -150,4 +150,10 @@ extern u64 kvm_nvhe_sym(id_aa64smfr0_el1_sys_val);
 extern unsigned long kvm_nvhe_sym(__icache_flags);
 extern unsigned int kvm_nvhe_sym(kvm_arm_vmid_bits);
 
+struct kvm_nvhe_clock_data {
+	u32 mult;
+	u32 shift;
+	u64 epoch_ns;
+	u64 epoch_cyc;
+};
 #endif /* __ARM64_KVM_HYP_H__ */
