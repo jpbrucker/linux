db914c558fae578be19075c2346291014f66b1fd
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-11-21 15:31:09 +0000

    iommu/io-pgtable-arm: Split initialization
    Extract the configuration part from io-pgtable-arm.c, move it to
    io-pgtable-arm-common.c.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- include/linux/io-pgtable-arm.h
+++ include/linux/io-pgtable-arm.h
@@ -171,20 +171,15 @@ static inline bool iopte_leaf(arm_lpae_iopte pte, int lvl,
 #define __arm_lpae_phys_to_virt	__va
 
 /* Generic functions */
-int arm_lpae_map_pages(struct io_pgtable_ops *ops, unsigned long iova,
-		       phys_addr_t paddr, size_t pgsize, size_t pgcount,
-		       int iommu_prot, gfp_t gfp, size_t *mapped);
-int arm_lpae_map(struct io_pgtable_ops *ops, unsigned long iova,
-		 phys_addr_t paddr, size_t size, int iommu_prot, gfp_t gfp);
-size_t arm_lpae_unmap_pages(struct io_pgtable_ops *ops, unsigned long iova,
-			    size_t pgsize, size_t pgcount,
-			    struct iommu_iotlb_gather *gather);
-size_t arm_lpae_unmap(struct io_pgtable_ops *ops, unsigned long iova,
-		      size_t size, struct iommu_iotlb_gather *gather);
-phys_addr_t arm_lpae_iova_to_phys(struct io_pgtable_ops *ops,
-				  unsigned long iova);
 void __arm_lpae_free_pgtable(struct arm_lpae_io_pgtable *data, int lvl,
 			     arm_lpae_iopte *ptep);
+
+int arm_lpae_init_pgtable(struct io_pgtable_cfg *cfg,
+			  struct arm_lpae_io_pgtable *data);
+int arm_lpae_init_pgtable_s1(struct io_pgtable_cfg *cfg,
+			     struct arm_lpae_io_pgtable *data);
+int arm_lpae_init_pgtable_s2(struct io_pgtable_cfg *cfg,
+			     struct arm_lpae_io_pgtable *data);
 
 /* Host/hyp-specific functions */
 void *__arm_lpae_alloc_pages(size_t size, gfp_t gfp, struct io_pgtable_cfg *cfg);
