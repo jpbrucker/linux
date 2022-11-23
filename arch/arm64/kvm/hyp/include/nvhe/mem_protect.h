9a073dc7e7cb9776ab27a70a42c2311a4ae32ec6
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-11-23 10:51:06 +0000

    KVM: arm64: pkvm: Add __pkvm_host_share/unshare_dma()
    Host pages mapped in the SMMU must not be donated to the guest or
    hypervisor, since the host could then use DMA to break confidentiality.
    Mark them shared in the host stage-2 page tables, and keep a refcount in
    the hyp vmemmap.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -58,6 +58,7 @@ enum pkvm_component_id {
 	PKVM_ID_HOST,
 	PKVM_ID_HYP,
 	PKVM_ID_GUEST,
+	PKVM_ID_IOMMU,
 };
 
 extern unsigned long hyp_nr_cpus;
@@ -72,6 +73,8 @@ int __pkvm_host_share_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu);
 int __pkvm_host_donate_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu);
 int __pkvm_guest_share_host(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
 int __pkvm_guest_unshare_host(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
+int __pkvm_host_share_dma(u64 phys_addr, size_t size, bool is_ram);
+int __pkvm_host_unshare_dma(u64 phys_addr, size_t size);
 
 bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
