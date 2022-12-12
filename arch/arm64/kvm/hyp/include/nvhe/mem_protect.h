caf8f78b1ed1f49eb91454eab3464f05f23f49fc
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-12-12 15:38:04 +0000

    KVM: arm64: pkvm: Expose pkvm_admit_host_page()
    Since the IOMMU driver will need admit_host_page(), make it non static.
    As a result we can drop refill_memcache() and call admit_host_page()
    directly from pkvm_refill_memcache().
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -83,8 +83,6 @@ void handle_host_mem_abort(struct kvm_cpu_context *host_ctxt);
 int hyp_pin_shared_mem(void *from, void *to);
 void hyp_unpin_shared_mem(void *from, void *to);
 void reclaim_guest_pages(struct pkvm_hyp_vm *vm, struct kvm_hyp_memcache *mc);
-int refill_memcache(struct kvm_hyp_memcache *mc, unsigned long min_pages,
-		    struct kvm_hyp_memcache *host_mc);
 
 void *pkvm_map_donated_memory(unsigned long host_va, size_t size);
 void pkvm_unmap_donated_memory(void *va, size_t size);
