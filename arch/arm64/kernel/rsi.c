71300713ade60286a58ed85b199e4239b15ca2a6
Steven Price <steven.price@arm.com>
2023-01-12 16:32:44 +0000

    arm64: Make the PHYS_MASK_SHIFT dynamic
    Make the PHYS_MASK_SHIFT dynamic for Realms. This is only is required
    for masking the PFN from a pte entry. For a realm phys_mask_shift is
    reduced if the RMM reports a smaller configured size for the guest.
    
    The realm configuration splits the address space into two with the top
    half being memory shared with the host, and the bottom half being
    protected memory. We treat the bit which controls this split as an
    attribute bit and hence exclude it (and any higher bits) from the mask.
    
    Co-developed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    
    ---
    v3: Drop the MAX_PHYS_MASK{,_SHIFT} definitions as they are no longer
    needed.

--- arch/arm64/kernel/rsi.c
+++ arch/arm64/kernel/rsi.c
@@ -13,6 +13,8 @@ struct realm_config config;
 unsigned long prot_ns_shared;
 EXPORT_SYMBOL(prot_ns_shared);
 
+unsigned int phys_mask_shift = CONFIG_ARM64_PA_BITS;
+
 DEFINE_STATIC_KEY_FALSE_RO(rsi_present);
 EXPORT_SYMBOL(rsi_present);
 
@@ -79,6 +81,9 @@ void __init arm64_rsi_init(void)
 	if (rsi_get_realm_config(&config))
 		return;
 	prot_ns_shared = BIT(config.ipa_bits - 1);
+
+	if (config.ipa_bits - 1 < phys_mask_shift)
+		phys_mask_shift = config.ipa_bits - 1;
 
 	static_branch_enable(&rsi_present);
 }
