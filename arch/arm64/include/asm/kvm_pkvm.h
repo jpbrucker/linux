6533e6dba36766184f01c1c23a4506b30eed8f44
Fuad Tabba <tabba@google.com>
2023-03-23 13:29:15 +0000

    KVM: arm64: Enable SVE for protected guests
    Now that the hypervisor has complete control over the guest's SVE
    state, allow protected guests to use SVE.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -77,6 +77,7 @@ void pkvm_destroy_hyp_vm(struct kvm *kvm);
  * - AArch64 guests only (no support for AArch32 guests):
  *	AArch32 adds complexity in trap handling, emulation, condition codes,
  *	etc...
+ * - SVE
  * - RAS (v1)
  *	Supported by KVM
  */
@@ -85,6 +86,7 @@ void pkvm_destroy_hyp_vm(struct kvm *kvm);
 	FIELD_PREP(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_EL1), ID_AA64PFR0_EL1_ELx_64BIT_ONLY) | \
 	FIELD_PREP(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_EL2), ID_AA64PFR0_EL1_ELx_64BIT_ONLY) | \
 	FIELD_PREP(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_EL3), ID_AA64PFR0_EL1_ELx_64BIT_ONLY) | \
+	FIELD_PREP(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_SVE), ID_AA64PFR0_EL1_SVE_IMP) | \
 	FIELD_PREP(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_RAS), ID_AA64PFR0_EL1_RAS_IMP) \
 	)
 
@@ -163,11 +165,19 @@ void pkvm_destroy_hyp_vm(struct kvm *kvm);
 	)
 
 /*
- * No support for Scalable Vectors for protected VMs:
- *	Requires additional support from KVM, e.g., context-switching and
- *	trapping at EL2
+ * No restrictions for Scalable Vectors (SVE).
  */
-#define PVM_ID_AA64ZFR0_ALLOW (0ULL)
+#define PVM_ID_AA64ZFR0_ALLOW (\
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_SVEver) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_AES) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_BitPerm) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_BF16) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_SHA3) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_SM4) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_I8MM) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_F32MM) | \
+	ARM64_FEATURE_MASK(ID_AA64ZFR0_EL1_F64MM) \
+	)
 
 /*
  * No support for debug, including breakpoints, and watchpoints for protected
