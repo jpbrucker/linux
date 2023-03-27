fbdaa1b364b0c8c38312b81a231233dcc89e8739
Suzuki K Poulose <suzuki.poulose@arm.com>
2023-03-27 11:58:58 +0100

    arm64: Detect if in a realm and set RIPAS RAM
    Detect that the VM is a realm guest by the presence of the RSI
    interface.
    
    If in a realm then all memory needs to be marked as RIPAS RAM initially,
    the loader may or may not have done this for us. To be sure iterate over
    all RAM and mark it as such. Any failure is fatal as that implies the
    RAM regions passed to Linux are incorrect - which would mean failing
    later when attempting to access non-existent RAM.
    
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Co-developed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>
    ---
    Changes since v3:
     * Provide safe/unsafe versions for converting memory to protected,
       using the safer version only for the early boot.
     * Use the new psci_early_test_conduit() function to avoid calling an
       SMC if EL3 is not present (or not configured to handle an SMC).
    Changes since v2:
     * Use DECLARE_STATIC_KEY_FALSE rather than "extern struct
       static_key_false".
     * Rename set_memory_range() to rsi_set_memory_range().
     * Downgrade some BUG()s to WARN()s and handle the condition by
       propagating up the stack. Comment the remaining case that ends in a
       BUG() to explain why.
     * Rely on the return from rsi_request_version() rather than checking
       the version the RMM claims to support.
     * Rename the generic sounding arm64_setup_memory() to
       arm64_rsi_setup_memory() and move the call site to setup_arch().

--- arch/arm64/include/asm/rsi_cmds.h
+++ arch/arm64/include/asm/rsi_cmds.h
@@ -10,6 +10,11 @@
 
 #include <asm/rsi_smc.h>
 
+enum ripas {
+	RSI_RIPAS_EMPTY,
+	RSI_RIPAS_RAM,
+};
+
 static inline unsigned long rsi_request_version(unsigned long req,
 						unsigned long *out_lower,
 						unsigned long *out_higher)
@@ -32,6 +37,23 @@ static inline unsigned long rsi_get_realm_config(struct realm_config *cfg)
 
 	arm_smccc_smc(SMC_RSI_REALM_CONFIG, virt_to_phys(cfg),
 		      0, 0, 0, 0, 0, 0, &res);
+	return res.a0;
+}
+
+static inline unsigned long rsi_set_addr_range_state(phys_addr_t start,
+						     phys_addr_t end,
+						     enum ripas state,
+						     unsigned long flags,
+						     phys_addr_t *top)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_smc(SMC_RSI_IPA_STATE_SET, start, end, state,
+		      flags, 0, 0, 0, &res);
+
+	if (top)
+		*top = res.a1;
+
 	return res.a0;
 }
 
