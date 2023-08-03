480a343517b1d0c0339c73d8b5ecfe9662cf9750
Steven Price <steven.price@arm.com>
2023-08-03 11:22:48 +0100

    arm64: rme: Enable PMU support with a realm guest
    Use the PMU registers from the RmiRecExit structure to identify when an
    overflow interrupt is due and inject it into the guest. Also hook up the
    configuration option for enabling the PMU within the guest.
    
    When entering a realm guest with a PMU interrupt pending, it is
    necessary to disable the physical interrupt. Otherwise when the RMM
    restores the PMU state the physical interrupt will trigger causing an
    immediate exit back to the host. The guest is expected to acknowledge
    the interrupt causing a host exit (to update the GIC state) which gives
    the opportunity to re-enable the physical interrupt before the next PMU
    event.
    
    Number of PMU counters is configured by the VMM by writing to PMCR.N.
    
    Signed-off-by: Steven Price <steven.price@arm.com>
    ---
    Changes since v2:
     * Add a macro kvm_pmu_get_irq_level() to avoid compile issues when PMU
       support is disabled.

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -15,6 +15,7 @@
 #include <linux/vmalloc.h>
 #include <linux/fs.h>
 #include <linux/mman.h>
+#include <linux/perf/arm_pmu.h>
 #include <linux/sched.h>
 #include <linux/kvm.h>
 #include <linux/kvm_irqfd.h>
@@ -1223,6 +1224,8 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 	run->exit_reason = KVM_EXIT_UNKNOWN;
 	run->flags = 0;
 	while (ret > 0) {
+		bool pmu_stopped = false;
+
 		/*
 		 * Check conditions before entering the guest
 		 */
@@ -1253,6 +1256,11 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 				      vcpu->arch.hw_mmu->arch);
 
 		kvm_pmu_flush_hwstate(vcpu);
+
+		if (vcpu_is_rec(vcpu) && kvm_pmu_get_irq_level(vcpu)) {
+			pmu_stopped = true;
+			arm_pmu_set_phys_irq(false);
+		}
 
 		local_irq_disable();
 
@@ -1355,6 +1363,9 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 		}
 
 		preempt_enable();
+
+		if (pmu_stopped)
+			arm_pmu_set_phys_irq(true);
 
 		/*
 		 * The ARMv8 architecture doesn't give the hypervisor
